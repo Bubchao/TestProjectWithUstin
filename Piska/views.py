9609448956cdef11dd96fca21109ab9b54a707f2
Dima Borisenok <mityaborisenok@mail.ru>
2023-10-25 22:00:55 +0300

    docker-compose.yml

--- Piska/views.py
+++ Piska/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+
+def get_data(request):
+    data = [
+        {'id': 1, 'name': 'Item 1'},
+        {'id': 2, 'name': 'Item 2'},
+        {'id': 3, 'name': 'Item 3'},
+        ]
